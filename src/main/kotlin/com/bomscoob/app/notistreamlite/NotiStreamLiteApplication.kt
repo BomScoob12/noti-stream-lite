39e10c63306a795e10ef8c3e56c4aeef4370226a
BomScoob12 <sorawitkraukham@gmail.com>
2025-07-31 17:17:03 +0700

    init

---
+++ src/main/kotlin/com/bomscoob/app/notistreamlite/NotiStreamLiteApplication.kt
@@ -0,0 +1,11 @@
+package com.bomscoob.app.notistreamlite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotiStreamLiteApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotiStreamLiteApplication>(*args)
+}
---
+++ src/test/kotlin/com/bomscoob/app/notistreamlite/NotiStreamLiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bomscoob.app.notistreamlite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotiStreamLiteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
